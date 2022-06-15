5686db0ae43dfce286a8ca6349b9d72f94ff04ec
manuelibarbia <manuelibarbia02@gmail.com>
2022-06-14 22:08:09 -0300

    Clase 14/06

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -1,39 +1,46 @@
 // local Storage va a guardar un arreglo de ciudades agregadas
-function saveInfoInLocalStorage() {
-    let campo_lleno = true;
-    let city_to_add = document.getElementById("city-to-add");
-    if (city_to_add.value === "") {
-        campo_lleno = false
-    }
 
-    if (campo_lleno) {
-        let P_success = document.createElement("p");
-        let P_success_content = document.createTextNode("Ciudad agregada con éxito");
-        P_success.appendChild(P_success_content);
-        let div_messages = document.getElementById("messages_add_city");
-        div_messages.appendChild(P_success);
-        P_success.setAttribute("class", "message success")
-    } else {
-        let P_error = document.createElement("p");
-        let P_error_content = document.createTextNode("La ciudad ingresada no se encuentra en la API o se produjo un error al consultar");
-        P_error.appendChild(P_error_content);
-        let div_messages = document.getElementById("messages_add_city");
-        div_messages.appendChild(P_error);
-        P_error.setAttribute("class", "message error")
-    }
+function saveInfoInLocalStorage() {
+    let newCity = document.getElementById("city-to-add");
+    localStorage.setItem("CITIES", JSON.stringify(newCity.value));
 }
 
-// function getCitiesFromLocalStorage() {
-//     let cities = localStorage.getItem("CITIES");
-//     if (cities) {
-//         cities = JSON.parse(cities);
-//     } else {
-//         cities = [];
+function getCitiesFromLocalStorage() {
+    let cities = localStorage.getItem("CITIES");
+    if (cities) {
+        cities = JSON.parse(cities);
+    } else {
+        cities = [];
+    }
+    return cities;
+}
+
+function addNewCityToLocalStorage(newCity) {
+    let cities = getCitiesFromLocalStorage();
+    cities.push(newCity);
+    localStorage.setItem("CITIES", JSON.stringify(cities));
+}
+
+// function validateInfo() {
+//     let campo_lleno = true;
+//     const city_to_add = document.getElementById("city-to-add");
+//     if (city_to_add.value === "") {
+//         campo_lleno = false
 //     }
-//     return cities;
-// }
-// function addNewCityToLocalStorage(newCity) {
-//     let cities = getCitiesFromLocalStorage();
-//     cities.push(newCity);
-//     localStorage.setItem(“CITIES”, JSON.stringify(cities));
+
+//     if (campo_lleno) {
+//         let P_success = document.createElement("p");
+//         let P_success_content = document.createTextNode("Ciudad agregada con éxito");
+//         P_success.appendChild(P_success_content);
+//         let div_messages = document.getElementById("messages_add_city");
+//         div_messages.appendChild(P_success);
+//         P_success.setAttribute("class", "message success")
+//     } else {
+//         let P_error = document.createElement("p");
+//         let P_error_content = document.createTextNode("La ciudad ingresada no se encuentra en la API o se produjo un error al consultar");
+//         P_error.appendChild(P_error_content);
+//         let div_messages = document.getElementById("messages_add_city");
+//         div_messages.appendChild(P_error);
+//         P_error.setAttribute("class", "message error")
+//     }
 // }
