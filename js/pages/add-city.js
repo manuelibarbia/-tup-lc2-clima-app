bdb7ea2ebcb83c81bd70e3e5df84a64ecdf427ed
manuelibarbia <manuelibarbia02@gmail.com>
2022-06-16 22:14:49 -0300

    Solucionado carteles infinitos, falta validar ciudades almacenadas

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -3,8 +3,12 @@ let cities = getCitiesFromLocalStorage();
 
 function addNewCityToLocalStorage() {
     let newCity = document.getElementById("city-to-add");
-    cities.push(newCity.value);
-    localStorage.setItem("CITIES", JSON.stringify(cities));
+    if (validateCity(newCity)) {
+        cities.push(newCity.value);
+        localStorage.setItem("CITIES", JSON.stringify(cities));
+    } else {
+        alert("La ciudad ya se encuentra almacenada")
+    }
 }
 
 function getCitiesFromLocalStorage() {
@@ -17,13 +21,39 @@ function getCitiesFromLocalStorage() {
     return cities;
 }
 
+
+function validateCity(newCity) {
+    list_validate = localStorage.getItem("CITIES")
+    list_validate = JSON.parse(list_validate)
+    if (list_validate != null) {
+        alert("La lista tiene contenido")
+        if (list_validate.includes(newCity.value)) {
+            return false
+        }else {
+            return true
+        }
+    }
+}
+
+// function validateCity(newCity) {
+//     list_validate = localStorage.getItem("CITIES")
+//     list_validate = JSON.parse(list_validate)
+//     for (i = 0; i<=list_validate.length; i++) {
+//         if (newCity.value === list_validate[i]) {
+//             alert("La ciudad ya está almacenada.")
+//             break
+//         }
+//     }
+// }
+
 // function validateInfo() {
 //     let campo_lleno = true;
 //     const city_to_add = document.getElementById("city-to-add");
 //     if (city_to_add.value === "") {
 //         campo_lleno = false
 //     }
-
+//     let div_messages = document.getElementById("messages_add_city")
+//     div_messages.innerHTML = ""
 //     if (campo_lleno) {
 //         let P_success = document.createElement("p");
 //         let P_success_content = document.createTextNode("Ciudad agregada con éxito");
