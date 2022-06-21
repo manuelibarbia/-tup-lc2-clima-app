12f7730f1e98af17fbbfd6b0c9afeb022a7b1a66
manuelibarbia <manuelibarbia02@gmail.com>
2022-06-21 16:41:43 -0300

    Implementando selector de ciudades

---
+++ js/common.js
@@ -0,0 +1,9 @@
+function getCitiesFromLocalStorage() {
+    let cities = localStorage.getItem("CITIES");
+    if (cities) {
+        cities = JSON.parse(cities);
+    } else {
+        cities = [];
+    }
+    return cities;
+}
---
+++ js/pages/index.js
@@ -0,0 +1,22 @@
+let selector = document.getElementById("select-city");
+console.log(selector)
+
+
+function getCitiesFromLocalStorage() {
+    let cities = localStorage.getItem("CITIES");
+    if (cities) {
+        cities = JSON.parse(cities);
+    } else {
+        cities = [];
+    }
+    return cities;
+}
+
+// function addCitiesToSelector () {
+//     let cities = getCitiesFromLocalStorage();
+    
+//     selector.innerHTML += `<option value="" disabled selected>Seleccionar ciudad</option>`
+//     for (let i = 0; i < cities.length; i++) {
+//         selector.innerHTML += `<option value="${cities[i]}">${cities[i]}</option>`
+//     }
+// }
