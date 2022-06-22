9ffee2c2cf38fbce14dec370df2eccd340ba12b1
manuelibarbia <manuelibarbia02@gmail.com>
2022-06-22 17:40:41 -0300

    Consultar mejorado, avisa si no hay ciudades

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,12 +1,26 @@
 const cities_select = document.getElementById('select-city');
-addCitiesToSelect(cities_select, cities);
+addCitiesToSelect();
 
-function addCitiesToSelect(cities_select, cities) {
-    for (i=0; i<cities.length; i++) {
-        let city_in_select = document.createElement("option")
-        let city_in_select_content = document.createTextNode(`${cities[i]}`);
-        city_in_select.appendChild(city_in_select_content);
-        city_in_select.setAttribute(`value`, `${cities[i]}`);
-        cities_select.appendChild(city_in_select)
+function addCitiesToSelect() {
+    if (cities == "") {
+        cities_select.innerHTML = `<option value="empty" disabled selected>No hay ciudades</option>`
+    } else {
+        for (i=0; i<cities.length; i++) {
+            cities_select.innerHTML += `<option value="${cities[i]}">${cities[i]}</option>`
+        }
     }
 }
+
+// function addCitiesToSelect() {
+//     if (cities == "") {
+//         cities_select.innerHTML = `<option value="empty" disabled selected>No hay ciudades</option>`
+//     } else {
+//         for (i=0; i<cities.length; i++) {
+//             let city_in_select = document.createElement("option")
+//             let city_in_select_content = document.createTextNode(`${cities[i]}`);
+//             city_in_select.appendChild(city_in_select_content);
+//             city_in_select.setAttribute(`value`, `${cities[i]}`);
+//             cities_select.appendChild(city_in_select)
+//         }
+//     }
+// }
