468a44b99b0102eeea808a42902a9c322fc21d18
manuelibarbia <manuelibarbia02@gmail.com>
2022-06-28 22:14:26 +0200

    Cambio función consultApi de common a index

--- js/common.js
+++ js/common.js
@@ -1,5 +1,4 @@
 let cities = getCitiesFromLocalStorage();
-const api_key = "ff0ed85d936b39b952700f07222bdfea";
 
 function getCitiesFromLocalStorage() {
     let cities = localStorage.getItem("CITIES");
@@ -9,44 +8,4 @@ function getCitiesFromLocalStorage() {
         cities = [];
     }
     return cities;
-}
-
-async function consultApi(city_to_consult) {
-    if (weather_result) {
-        weather_result.innerHTML = "";
-    }
-    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_to_consult}&appid=${api_key}&units=metric&lang=es`)
-        .then((response) => {
-            return response.json();
-        })
-        .then(data => {
-            weatherInfo(data);
-        })
-        .catch((error) => {
-            console.error("Se ha encontrado un error: "+ error);
-            return false;
-        });
-}
-
-function weatherInfo(data) {
-    let city = data.name;
-    let icon = data.weather[0].icon;
-    let temp = data.main.temp;
-    let feels_like = data.main.feels_like;
-    let humidity = data.main.humidity;
-    let wind_speed = data.wind.speed;
-    let pressure = data.main.pressure;
-
-    let card = `<div class="card">
-                    <h3>${city}</h3>
-                    <img src="http://openweathermap.org/img/wn/${icon}.png" alt="Imagen">
-                    <p>Temperatura: ${temp}°</p>
-                    <p>Sensación Térmica: ${feels_like}°</p>
-                    <p>Humedad: ${humidity}%</p>
-                    <p>Velocidad del Viento: ${wind_speed}km/h</p>
-                    <p>Presión: ${pressure} P</p>
-                </div>`
-    if (weather_result) {
-        weather_result.innerHTML += card;
-    }
 }
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,5 +1,5 @@
 const cities_select = document.getElementById('select-city');
-const button = document.getElementById("button-consult");
+const api_key = "ff0ed85d936b39b952700f07222bdfea";
 const weather_result = document.getElementById("section-weather-result");
 
 addCitiesToSelect();
@@ -11,5 +11,45 @@ function addCitiesToSelect() {
         for (i=0; i<cities.length; i++) {
             cities_select.innerHTML += `<option value="${cities[i]}">${cities[i]}</option>`
         }
+    }
+}
+
+async function consultApi(city_to_consult) {
+    if (weather_result) {
+        weather_result.innerHTML = "";
+    }
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_to_consult}&appid=${api_key}&units=metric&lang=es`)
+        .then((response) => {
+            return response.json();
+        })
+        .then(data => {
+            weatherInfo(data);
+        })
+        .catch((error) => {
+            console.error("Se ha encontrado un error: "+ error);
+            return false;
+        });
+}
+
+function weatherInfo(data) {
+    let city = data.name;
+    let icon = data.weather[0].icon;
+    let temp = data.main.temp;
+    let feels_like = data.main.feels_like;
+    let humidity = data.main.humidity;
+    let wind_speed = data.wind.speed;
+    let pressure = data.main.pressure;
+
+    let card = `<div class="card">
+                    <h3>${city}</h3>
+                    <img src="http://openweathermap.org/img/wn/${icon}.png" alt="Imagen">
+                    <p>Temperatura: ${temp}°</p>
+                    <p>Sensación térmica: ${feels_like}°</p>
+                    <p>Humedad: ${humidity}%</p>
+                    <p>Velocidad del viento: ${wind_speed}km/h</p>
+                    <p>Presión: ${pressure} P</p>
+                </div>`
+    if (weather_result) {
+        weather_result.innerHTML += card;
     }
 }
